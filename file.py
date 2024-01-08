e46669826982e4725728fed9a45399e06d6c5122
Lawrence Ma <lawrencema415@gmail.com>
2024-01-08 15:35:08 -0800

    Regex testing

--- file.py
+++ file.py
@@ -1,12 +1,21 @@
 from urllib.request import urlopen
+import re
 
 print("Python practice for web scraping")
 
-url = "http://olympus.realpython.org/profiles/aphrodite"
+url = "http://olympus.realpython.org/profiles/poseidon"
 page = urlopen(url)
 page
 
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 
-print(html)
+# print(html)
+
+title_index = html.find("<title>")
+# 14
+
+match_results = re.search("ab*c", "ABC", re.IGNORECASE)
+match_results.group()
+
+print(match_results)
