c3a48e75341d159a3a5829fbb519d35c2f05aa90
Lawrence Ma <lawrencema415@gmail.com>
2024-01-08 15:38:26 -0800

    Decode and extract

--- file.py
+++ file.py
@@ -1,21 +1,31 @@
 from urllib.request import urlopen
-import re
+# import re
 
-print("Python practice for web scraping")
+# print("Python practice for web scraping")
 
-url = "http://olympus.realpython.org/profiles/poseidon"
-page = urlopen(url)
-page
+# url = "http://olympus.realpython.org/profiles/poseidon"
+# page = urlopen(url)
+# page
 
-html_bytes = page.read()
-html = html_bytes.decode("utf-8")
+# html_bytes = page.read()
+# html = html_bytes.decode("utf-8")
 
 # print(html)
 
-title_index = html.find("<title>")
+# title_index = html.find("<title>")
 # 14
 
-match_results = re.search("ab*c", "ABC", re.IGNORECASE)
-match_results.group()
+url = "http://olympus.realpython.org/profiles/dionysus"
+html_page = urlopen(url)
+html_text = html_page.read().decode("utf-8")
 
-print(match_results)
+for string in ["Name: ", "Favorite Color:"]:
+    string_start_idx = html_text.find(string)
+    text_start_idx = string_start_idx + len(string)
+
+    next_html_tag_offset = html_text[text_start_idx:].find("<")
+    text_end_idx = text_start_idx + next_html_tag_offset
+
+    raw_text = html_text[text_start_idx : text_end_idx]
+    clean_text = raw_text.strip(" \r\n\t")
+    print(clean_text)
